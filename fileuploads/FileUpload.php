96307959dee60229038fdaeaed27b1afaaa0fe2d
SummerMineJack <hjzxzone@163.com>
2018-12-26 17:20:44 +0800

    PHP教程之JSON

--- fileuploads/FileUpload.php
+++ fileuploads/FileUpload.php
@@ -20,7 +20,18 @@ if ((($_FILES['file']['type'] == "image/jpg") || ($_FILES['file']['type'] == "im
         echo "上传文件名：" . $_FILES['file']['name'] . "<br>";
         echo "上传文件类型：" . $_FILES['file']['type'] . "<br>";
         echo "上传文件大小：" . ($_FILES['file']['size']) . "kb<br>";
-        echo "文件临时存储位置；" . $_FILES['file']['tmp_name'];
+        echo "文件临时存储位置；" . $_FILES['file']['tmp_name'] . "<br>";
+        //将上传的文件保存到特定的位置
+        $dir = iconv("UTF-8", "GBK", "upload");
+        if (!file_exists($dir)) {
+            mkdir($dir, 0777, true);
+        }
+        if (file_exists("upload/" . $_FILES['file']['name'])) {
+            echo $_FILES['file']['name'] . "文件已存在";
+        } else {
+            move_uploaded_file($_FILES['file']['tmp_name'], "upload/" . $_FILES['file']['name']);
+            echo "文件存储在" . "upload/" . $_FILES['file']['name'];
+        }
     }
 } else if ($_FILES['file']['size'] > 2048000) {
     echo "仅支持文件大小为" . (2048000 / 1024) . "kb";
