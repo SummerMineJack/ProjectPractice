9ba6c4821cd0dd2251314d87bd3276bef8b1e809
SummerMineJack <hjzxzone@163.com>
2019-01-03 09:42:07 +0800

    登录页面修改

--- login/MysqlConntect.php
+++ login/MysqlConntect.php
@@ -1,15 +1,25 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Summer
  * Date: 2018/12/27
  * Time: 20:36
  */
-class mysqlConntect{
+class mysqlConntect {
     private $mysqli;
     private $result;
-    public function conntct(){
-        $this->mysqli=new mysqli();
 
+    public function conntct() {
+        $this->mysqli = new mysqli("localhost", "root", "hj1649789..", "userinfo", "3306");
+    }
+
+    /**
+     * 插入
+     */
+    public function insertValue($table, $data) {
+        $sql = "insert into" . $table . " (username,userpwd) values ('{$data['username']}','{$data['userpwd']}')";
+        $this->mysqli->query($sql);
+        return $this->mysqli->insert_id;
     }
 }
--- login/userRegist.php
+++ login/userRegist.php
@@ -5,3 +5,9 @@
  * Date: 2018/12/27
  * Time: 13:44
  */
+require_once "MysqlConntect.php";
+$mysql = new mysqlConntect();
+$mysql->conntct();
+
+$username = $_POST['UserName'];
+$userpdw = $_POST['UserPassword'];
